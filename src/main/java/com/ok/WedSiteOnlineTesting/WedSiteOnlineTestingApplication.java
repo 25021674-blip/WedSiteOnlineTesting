7409f576aaa3ffdba60722e349368bacc13a5350
Doanh <25021674@vnu.edu.vn>
2026-07-03 22:09:28 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/ok/WedSiteOnlineTesting/WedSiteOnlineTestingApplication.java
@@ -0,0 +1,13 @@
+package com.ok.WedSiteOnlineTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedSiteOnlineTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedSiteOnlineTestingApplication.class, args);
+	}
+
+}
